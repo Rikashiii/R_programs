db388fe4306d8269c1828c1e556111aaf692256f
Harsh Chandrakar <theharshchandrakar@outlook.com>
2025-10-06 15:48:45 +0530

    learned new programs

---
+++ isnullCheck.R
@@ -0,0 +1,14 @@
+d <- NA
+is.na(d)      #checks if value is NA(Null)
+
+g <- c(34, NA, 67, NA, 89, 20, NA)
+is.na(g)      #gives vector of boolean values, with TRUE if a value is NA
+sum(is.na(g)) #FALSE=0, TRUE=1, adds the vector and gives the sum
+
+#0 div by
+w <- 0/0
+w           #gives output NaN(not a number) instead of 0
+is.nan(w)
+is.na(w)
+is.finite(w)
+is.infinite(w)
