9a9d01f11f47f1a401df4e2ddd416c2bbce440ae
Harsh Chandrakar <theharshchandrakar@outlook.com>
2025-10-06 16:03:00 +0530

    modified matrix

--- matrix.R
+++ matrix.R
@@ -1,10 +1,12 @@
 #Matrix
 
+#matrix(data, rows, columns)
+
 m = matrix(18, 3, 2)
 m
 
 m = matrix(c(1,2,3,4,5), 3, 2)
 m
 
-m = matrix(c(1,3,5,8,9,6),3,5, byrow = TRUE)
+m = matrix(c(1,3,5,8,9,6),7,5, byrow = TRUE)
 m
