a9e2692c77232ce785a7ad015978f18f571a9fcd
Harsh Chandrakar <theharshchandrakar@outlook.com>
2025-10-06 18:22:18 +0530

    select statement

--- views.R
+++ views.R
@@ -4,5 +4,10 @@ housing <- read.csv("D:/Datasets/Housing.csv")
 #conditional statement (where driveway is yes)
 ss_house <- subset(housing, driveway == "yes")
 View(ss_house)
+
 ss_house <- subset(housing, driveway == "yes" & price > 45000)
+View(ss_house)
+
+ss_house <- subset(housing, driveway == "yes" & price > 45000,
+                   select = c(price, bedrooms, recroom))    #select the columns to display
 View(ss_house)
