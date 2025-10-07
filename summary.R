425bbbfd1aa4d8681421f7f7b09aa12586842c63
Harsh Chandrakar <theharshchandrakar@outlook.com>
2025-10-07 15:42:47 +0530

    update summary.R

--- summary.R
+++ summary.R
@@ -1,4 +1,4 @@
-housing <- read.csv("D:/Datasets/Housing.csv")
+housing <- read.csv("D:/Datasets/Housing.csv", stringsAsFactors = T)
 summary(housing$price)
 
 s <- seq(2, 40, 2)
@@ -6,3 +6,16 @@ summary(s)
 
 summary(housing$recroom)
 summary(housing)
+
+summary(housing$price)
+
+attach(housing)
+summary(price)
+table(recroom)
+
+detach(housing)
+
+summary(price)
+summary(bedrooms)
+summary(lotsize)
+table(recroom)
