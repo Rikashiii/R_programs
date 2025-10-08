3e92ad9935a6a74e1695016735d0bed3c5b5823e
Harsh Chandrakar <theharshchandrakar@outlook.com>
2025-10-08 15:57:12 +0530

    updated deplyr_2

--- dplyr_2.R
+++ dplyr_2.R
@@ -10,3 +10,9 @@ grp_t <- group_by(cars2018, Transmission)
 summarise(grp_t, mu_price = mean(MPG),
           sd_price=sd(MPG),
           med_cyl = median(Cylinders))
+
+ss_cars <- filter(cars2018, Transmission=="Manual")
+ss_cars <- select(filter(cars2018, Transmission=="Manual"),
+                  Model, Displacement, MPG, Transmission)
+View(ss_cars)
+
