63fd758d23e82b326734db262c1d0a6f2202204c
Harsh Chandrakar <theharshchandrakar@outlook.com>
2025-10-08 15:01:07 +0530

    updated dplyr.R

--- dplyr.R
+++ dplyr.R
@@ -5,8 +5,25 @@ cars2018 <- read.csv("D:/Datasets/cars2018.csv", stringsAsFactors = TRUE)
 s_cars <- arrange(cars2018, Displacement)
 s_cars <- arrange(cars2018, Transmission )
 
-#sorting column insid column
+#sorting column inside column
 items <- read.csv("Items.csv")
 s_items <- arrange(items, Item.Type, Price)
 #descending
 s_items <- arrange(items, Item.Type, desc(Price))
+
+sel_cars <- select(cars2018, Model.Index, Gears, Aspiration)
+
+#select Model to Transmission column select
+sel_cars <- select(cars2018, Model:Transmission)
+
+#select column1 to column6 select
+sel_cars <- select(cars2018, 1:6)
+
+#select columns except column1 to column6
+sel_cars <- select(cars2018, -c(1:6))
+
+#select columns except Gear column
+sel_cars <- select(cars2018, -Gears)
+
+#select columns except Model to Transmission
+sel_cars <- select(cars2018, -c(Model:Transmission))
